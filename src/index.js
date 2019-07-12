c4fe6e68d85a22414230138f8359b108ede5721c
Romil Khanna <romilkhanna@outlook.com>
2019-07-12 02:08:17 -0400

    Add index file that was missing

---
+++ src/index.js
@@ -0,0 +1,93 @@
+import '@babel/polyfill';
+import express from 'express';
+import expressWinston from 'express-winston';
+import winston from 'winston';
+import morgan from 'morgan';
+import log from 'fancy-log';
+import expressValidator from 'express-validator';
+import bodyParser from 'body-parser';
+import compression from 'compression';
+import helmet from 'helmet';
+import cors from 'cors';
+import router from './routes';
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const app = express();
+const corsOptions = {
+  credentials: true,
+  origin: [],
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+app.use(cors(corsOptions));
+
+// compression and header security middleware
+app.use(compression());
+app.use(helmet());
+
+app.use(morgan('dev'));
+
+app.use(
+  bodyParser.urlencoded({
+    limit: '50mb',
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
+app.use(expressValidator());
+
+app.use(
+  expressWinston.logger({
+    transports: [new winston.transports.Console()],
+    meta: false,
+    expressFormat: true,
+    colorize: true,
+    format: winston.format.combine(winston.format.colorize(), winston.format.simple()),
+  })
+);
+
+app.use('/stripe/charge', express.static(`${__dirname}/public`));
+
+app.use(router);
+
+app.use((req, res, next) => {
+  if (
+    res.locals.data &&
+    (res.locals.data.length || res.locals.data.count || Object.keys(res.locals.data).length)
+  ) {
+    return res.json({
+      status: true,
+      data: res.locals.data,
+    });
+  }
+  return next();
+});
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Resource does not exist');
+  err.status = 404;
+  return next(err);
+});
+
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+  if (!isProduction) log(err.stack);
+
+  // eslint-disable-line no-unused-vars
+  return res.status(err.status || 500).json({
+    error: {
+      message: err.message,
+      error: isProduction ? {} : err,
+    },
+    status: false,
+  });
+});
+
+// configure port and listen for requests
+const port = parseInt(process.env.NODE_ENV === 'test' ? 8378 : process.env.PORT, 10) || 5000;
+export const server = app.listen(port, () => {
+  log(`Server is running on http://localhost:${port} `);
+});
+
+export default app;
